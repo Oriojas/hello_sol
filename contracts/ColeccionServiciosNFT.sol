b1824ff9f855db534410dfc6d742b554fb8a6002
Oscar ThinkPad <oscarriojas@gmail.com>
2025-11-21 14:58:38 -0500

    test/ contract deploy

--- contracts/ColeccionServiciosNFT.sol
+++ contracts/ColeccionServiciosNFT.sol
@@ -1,20 +1,10 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.5.0
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.20;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ColeccionServiciosNFT is
-    ERC721,
-    ERC721Enumerable,
-    ERC721URIStorage,
-    ERC721Burnable,
-    Ownable
-{
+contract ColeccionServiciosNFT is ERC721, ERC721URIStorage {
     // Estados posibles para cada servicio (enteros 1-5)
     // 1 = CREADO, 2 = ENCONTRADO, 3 = TERMINADO, 4 = CALIFICADO, 5 = PAGADO
 
@@ -51,9 +41,9 @@ contract ColeccionServiciosNFT is
         uint256 indexed tokenIdEvidencia
     );
 
-    constructor(
-        address initialOwner
-    ) ERC721("ColeccionServiciosNFT", "CSNFT") Ownable(initialOwner) {}
+    constructor() ERC721("ColeccionServiciosNFT", "CSNFT") {
+        _nextTokenId = 0;
+    }
 
     /**
      * @dev Crea un nuevo servicio (NFT) para un destinatario
@@ -61,6 +51,8 @@ contract ColeccionServiciosNFT is
      * @return tokenId del nuevo servicio creado
      */
     function crearServicio(address destinatario) public returns (uint256) {
+        require(destinatario != address(0), "Destinatario invalido");
+
         uint256 tokenId = _nextTokenId++;
         _safeMint(destinatario, tokenId);
 
@@ -70,7 +62,9 @@ contract ColeccionServiciosNFT is
         acompanantesServicios[tokenId] = address(0);
 
         // Asignar URI inicial basada en el estado
-        _setTokenURI(tokenId, URIsPorEstado[1]);
+        if (bytes(URIsPorEstado[1]).length > 0) {
+            _setTokenURI(tokenId, URIsPorEstado[1]);
+        }
 
         emit ServicioCreado(tokenId, destinatario);
         return tokenId;
@@ -79,15 +73,15 @@ contract ColeccionServiciosNFT is
     /**
      * @dev Cambia el estado de un servicio específico
      * @param tokenId ID del servicio a modificar
-     * @param nuevoEstado Nuevo estado del servicio
-     * @param calificacion Calificación del servicio (1-5, solo para estado CALIFICADO)
+     * @param nuevoEstado Nuevo estado del servicio (1-5)
+     * @param calificacion Calificación del servicio (1-5, solo para estado 4)
      */
     function cambiarEstadoServicio(
         uint256 tokenId,
         uint8 nuevoEstado,
         uint8 calificacion
     ) public {
-        require(ownerOf(tokenId) != address(0), "Servicio no existe");
+        require(_ownerOf(tokenId) != address(0), "Servicio no existe");
         require(nuevoEstado >= 1 && nuevoEstado <= 5, "Estado invalido");
 
         uint8 estadoAnterior = estadosServicios[tokenId];
@@ -127,7 +121,9 @@ contract ColeccionServiciosNFT is
             acompanantesServicios[tokenIdEvidencia] = acompanante;
 
             // Asignar URI de evidencia pagada
-            _setTokenURI(tokenIdEvidencia, URIsPorEstado[5]);
+            if (bytes(URIsPorEstado[5]).length > 0) {
+                _setTokenURI(tokenIdEvidencia, URIsPorEstado[5]);
+            }
 
             // Registrar relación entre token original y evidencia
             evidenciasServicios[tokenId] = tokenIdEvidencia;
@@ -136,14 +132,16 @@ contract ColeccionServiciosNFT is
         }
 
         // Actualizar URI según el nuevo estado
-        _setTokenURI(tokenId, URIsPorEstado[nuevoEstado]);
+        if (bytes(URIsPorEstado[nuevoEstado]).length > 0) {
+            _setTokenURI(tokenId, URIsPorEstado[nuevoEstado]);
+        }
 
         emit EstadoCambiado(tokenId, estadoAnterior, nuevoEstado, calificacion);
     }
 
     /**
      * @dev Configura la URI para un estado específico
-     * @param estado Estado para el cual configurar la URI
+     * @param estado Estado para el cual configurar la URI (1-5)
      * @param nuevaURI Nueva URI para el estado
      */
     function configurarURIEstado(uint8 estado, string memory nuevaURI) public {
@@ -155,12 +153,12 @@ contract ColeccionServiciosNFT is
     /**
      * @dev Obtiene el estado actual de un servicio
      * @param tokenId ID del servicio
-     * @return Estado actual del servicio
+     * @return Estado actual del servicio (1-5)
      */
     function obtenerEstadoServicio(
         uint256 tokenId
     ) public view returns (uint8) {
-        require(ownerOf(tokenId) != address(0), "Servicio no existe");
+        require(_ownerOf(tokenId) != address(0), "Servicio no existe");
         return estadosServicios[tokenId];
     }
 
@@ -172,7 +170,7 @@ contract ColeccionServiciosNFT is
     function obtenerCalificacionServicio(
         uint256 tokenId
     ) public view returns (uint8) {
-        require(ownerOf(tokenId) != address(0), "Servicio no existe");
+        require(_ownerOf(tokenId) != address(0), "Servicio no existe");
         return calificacionesServicios[tokenId];
     }
 
@@ -182,7 +180,7 @@ contract ColeccionServiciosNFT is
      * @param acompanante Dirección del acompañante
      */
     function asignarAcompanante(uint256 tokenId, address acompanante) public {
-        require(ownerOf(tokenId) != address(0), "Servicio no existe");
+        require(_ownerOf(tokenId) != address(0), "Servicio no existe");
         require(acompanante != address(0), "Acompanante no valido");
         acompanantesServicios[tokenId] = acompanante;
     }
@@ -193,7 +191,7 @@ contract ColeccionServiciosNFT is
      * @return Dirección del acompañante
      */
     function obtenerAcompanante(uint256 tokenId) public view returns (address) {
-        require(ownerOf(tokenId) != address(0), "Servicio no existe");
+        require(_ownerOf(tokenId) != address(0), "Servicio no existe");
         return acompanantesServicios[tokenId];
     }
 
@@ -205,7 +203,7 @@ contract ColeccionServiciosNFT is
     function obtenerEvidenciaServicio(
         uint256 tokenId
     ) public view returns (uint256) {
-        require(ownerOf(tokenId) != address(0), "Servicio no existe");
+        require(_ownerOf(tokenId) != address(0), "Servicio no existe");
         return evidenciasServicios[tokenId];
     }
 
@@ -214,7 +212,7 @@ contract ColeccionServiciosNFT is
      * @param tokenId ID del servicio a marcar como pagado
      */
     function marcarComoPagado(uint256 tokenId) public {
-        require(ownerOf(tokenId) != address(0), "Servicio no existe");
+        require(_ownerOf(tokenId) != address(0), "Servicio no existe");
         require(
             estadosServicios[tokenId] == 4, // CALIFICADO
             "Servicio debe estar calificado"
@@ -238,7 +236,9 @@ contract ColeccionServiciosNFT is
         acompanantesServicios[tokenIdEvidencia] = acompanante;
 
         // Asignar URI de evidencia pagada
-        _setTokenURI(tokenIdEvidencia, URIsPorEstado[5]);
+        if (bytes(URIsPorEstado[5]).length > 0) {
+            _setTokenURI(tokenIdEvidencia, URIsPorEstado[5]);
+        }
 
         // Registrar relación entre token original y evidencia
         evidenciasServicios[tokenId] = tokenIdEvidencia;
@@ -253,24 +253,6 @@ contract ColeccionServiciosNFT is
     }
 
     /**
-     * @dev Obtiene todos los servicios de un usuario
-     * @param usuario Dirección del usuario
-     * @return Array con los IDs de los servicios del usuario
-     */
-    function serviciosPorUsuario(
-        address usuario
-    ) public view returns (uint256[] memory) {
-        uint256 balance = balanceOf(usuario);
-        uint256[] memory tokens = new uint256[](balance);
-
-        for (uint256 i = 0; i < balance; i++) {
-            tokens[i] = tokenOfOwnerByIndex(usuario, i);
-        }
-
-        return tokens;
-    }
-
-    /**
      * @dev Obtiene la URI actual de un servicio
      * @param tokenId ID del servicio
      * @return URI actual del servicio
@@ -278,26 +260,19 @@ contract ColeccionServiciosNFT is
     function obtenerURIServicio(
         uint256 tokenId
     ) public view returns (string memory) {
-        require(ownerOf(tokenId) != address(0), "Servicio no existe");
+        require(_ownerOf(tokenId) != address(0), "Servicio no existe");
         return tokenURI(tokenId);
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _update(
-        address to,
-        uint256 tokenId,
-        address auth
-    ) internal override(ERC721, ERC721Enumerable) returns (address) {
-        return super._update(to, tokenId, auth);
+    /**
+     * @dev Obtiene el siguiente tokenId que se generará
+     * @return Próximo tokenId
+     */
+    function obtenerProximoTokenId() public view returns (uint256) {
+        return _nextTokenId;
     }
 
-    function _increaseBalance(
-        address account,
-        uint128 value
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._increaseBalance(account, value);
-    }
+    // Overrides required by Solidity
 
     function tokenURI(
         uint256 tokenId
@@ -307,12 +282,22 @@ contract ColeccionServiciosNFT is
 
     function supportsInterface(
         bytes4 interfaceId
-    )
-        public
-        view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
-        returns (bool)
-    {
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
+    }
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721) {
+        super._increaseBalance(account, value);
     }
 }
